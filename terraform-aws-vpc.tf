e549f18d31563b39354bf35044211da0cb5d67e3
Rajani45 <163117857+Rajani45@users.noreply.github.com>
2024-10-21 09:25:27 +0000

    Update terraform-aws-vpc.tf

--- terraform-aws-vpc.tf
+++ terraform-aws-vpc.tf
@@ -15,7 +15,7 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "main" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"  # Change as needed
+  availability_zone = "us-west-1a"  # Change as needed
 
   tags = {
     Name = "MainSubnet"
