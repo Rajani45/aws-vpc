a7683384bab3940e27f68baa1ba26c7e1e7f37e0
Rajani45 <163117857+Rajani45@users.noreply.github.com>
2024-10-22 11:52:48 +0000

    Update terraform-aws-vpc.tf

--- terraform-aws-vpc.tf
+++ terraform-aws-vpc.tf
@@ -30,3 +30,22 @@ resource "aws_internet_gateway" "main" {
     Name = "MainInternetGateway"
   }
 }
+# Associate Route Table with Subnet
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
+# Create Route Table
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "MainRouteTable"
+  }
+}
+
